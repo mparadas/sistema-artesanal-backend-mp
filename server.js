4b4f5d02770dae23a6674db47482f44856b9c104
mparadas <mparadas@gmail.com>
2026-02-27 02:21:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -31,19 +31,14 @@ const createMailTransporter = () =>
       pass: process.env.SMTP_PASS || ''
     }
   });
+
+ Configuración CORS mejorada para móvil
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*', 
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://192.168.100.224:5173', 'http://192.168.100.224:3000','http://localhost:10000', 'http://agromae_b.onrender.com' ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-// Configuración CORS mejorada para móvil
-//app.use(cors({
-  //origin: ['http://localhost:3000', 'http://localhost:5173', 'http://192.168.100.224:5173', 'http://192.168.100.224:3000','http://localhost:10000', 'http://agromae_b.onrender.com' ],
-  //credentials: true,
-  //methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  //allowedHeaders: ['Content-Type', 'Authorization']
-//}));
 
 app.use(express.json({ limit: '200kb' }));
 app.use(middlewareAuditoria); // Middleware para capturar información de auditoría
